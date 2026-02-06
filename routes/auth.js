6234d5d11f2b69fd19d521700c9fa5f6ca4da482
681540005009-7-ui <681540005009-7@ksu.ac.th>
2026-02-06 15:20:38 +0700

    add auth and category routes

--- routes/auth.js
+++
@@ -1,9 +0,0 @@
-// import ...
-const express = require('express')
-const router = express.Router()
-
-router.get('/regsiter',(req,res)=>{
-    res.send('Hello Register')
-})
-
-module.exports = router
--- server.js
+++ server.js
@@ -3,24 +3,18 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 
-const authRouter = require('./routes/auth')
-const categoryRouter = require('./routes/category')
-
 //middleware
 app.use(morgan('dev'))
 app.use(express.json())
 
-app.use('/api',authRouter)
-app.use('/api',category)
 
 // Step 3
 app.post('/api',(req,res)=>{
-    //code 
+    //code
     const { username,password } = req.body
     console.log(username,password)
-    res.send('Hello world')
+    res.send('Hello World')
 })
-
 
 // Step 2
 app.listen(5001,()=> console.log("Server Running is on port 5001"))
